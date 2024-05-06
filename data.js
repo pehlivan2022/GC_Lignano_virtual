7283ac639ca77b6139a6df421dd9b219a7a51759
pehlivan2022 <99028696+pehlivan2022@users.noreply.github.com>
2024-05-07 01:06:30 +0200

    gc_virtual

---
+++ data.js
@@ -0,0 +1,233 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-golf-club-lignano-high",
+      "name": "GOLF CLUB LIGNANO HIGH",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.03748626472726713,
+        "pitch": 1.403923611299625,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.037393573027250326,
+          "pitch": 1.4038309195996082,
+          "rotation": 0,
+          "target": "1-golf-club-lignano-low"
+        },
+        {
+          "yaw": -1.6068661397355015,
+          "pitch": 0.15206603041131572,
+          "rotation": 0,
+          "target": "3-hotel-italia-palace"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.5934928836964026,
+          "pitch": 0.06371971497276085,
+          "title": "HOTEL ITALIA PALACE",
+          "text": "hotelitaliapalace.it<div><br></div>"
+        }
+      ]
+    },
+    {
+      "id": "1-golf-club-lignano-low",
+      "name": "GOLF CLUB LIGNANO LOW",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.7078084354309837,
+        "pitch": 0.5782811400813319,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.31655485765289804,
+          "pitch": -1.2192818001894388,
+          "rotation": 0,
+          "target": "0-golf-club-lignano-high"
+        },
+        {
+          "yaw": -2.8259450468376315,
+          "pitch": 0.9527063045596442,
+          "rotation": 0.7853981633974483,
+          "target": "2-gc-lignano-16"
+        },
+        {
+          "yaw": -0.12072523188748896,
+          "pitch": 0.047757287507893764,
+          "rotation": 0,
+          "target": "3-hotel-italia-palace"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-gc-lignano-16",
+      "name": "GC LIGNANO 16",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.3690225152197097,
+        "pitch": 0.5909845917102814,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-hotel-italia-palace",
+      "name": "HOTEL ITALIA PALACE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.544212641242292,
+        "pitch": 1.5057407280485284,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.007714908260769704,
+          "pitch": 0.048903966362470186,
+          "rotation": 0,
+          "target": "0-golf-club-lignano-high"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.544212641242292,
+          "pitch": 1.5057407280485284,
+          "title": "Hotel Italia Palace",
+          "text": "BENVENUTI !"
+        }
+      ]
+    },
+    {
+      "id": "4-nostra-spiaggia",
+      "name": "NOSTRA SPIAGGIA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.2579626563494486,
+        "pitch": 1.5707963267948966,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.09131074837634756,
+          "pitch": 0.06500685940102358,
+          "rotation": 0,
+          "target": "0-golf-club-lignano-high"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.7899170306295282,
+          "pitch": 1.0932963261328474,
+          "title": "SPIAGGIA",
+          "text": ""
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
